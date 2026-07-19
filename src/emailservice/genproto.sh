f9d533d33eb4e7b105e55f1398a15a9185e95d3a
Darsh1199 <143002094+Darsh1199@users.noreply.github.com>
2026-07-19 17:15:07 +0530

    removing unnecessary comment in genproto.sh

--- src/emailservice/genproto.sh
+++ src/emailservice/genproto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
-#
+
 # Copyright 2018 Google LLC
-#
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 # [START gke_emailservice_genproto]
 
 python -m grpc_tools.protoc -I../../protos --python_out=. --grpc_python_out=. ../../protos/demo.proto
