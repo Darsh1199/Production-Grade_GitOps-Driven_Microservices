471b499312912f42cbb0171b49efe56f9a3b410e
“username” <emailaddr>
2026-07-19 19:57:53 +0530

    Add GHCR image pull secret

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,12 +1,18 @@
 terraform {
+  backend "s3" {
+    bucket = "devopsdock-terraform-backend-bucket-popo"
+    key    = "s3-backend"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.28.0"
+      version = "6.54.0"
     }
   }
+ 
 }
-
 provider "aws" {
   region = "us-east-1"
 }
